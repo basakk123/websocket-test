ed890583378145dc2543d2eb15dc1d572da7d9aa
dding ha <basakk@naver.com>
2022-10-03 18:46:27 +0900

    푸쉬 알림 카운트 완료

--- src/main/java/com/example/websocket/WebSocketService.java
+++ src/main/java/com/example/websocket/WebSocketService.java
@@ -9,23 +9,25 @@ import dto.ResponseMessage;
 @Service
 public class WebSocketService {
 	
-	private SimpMessagingTemplate messagingTemplate;
+	private final SimpMessagingTemplate messagingTemplate;
+	private final NotificationService notificationService;
 	
 	@Autowired
-	public WebSocketService(SimpMessagingTemplate messagingTemplate) {
+	public WebSocketService(SimpMessagingTemplate messagingTemplate, NotificationService notificationService) {
 		this.messagingTemplate =messagingTemplate;
+		this.notificationService = notificationService;
 	}
 	
 	public void notifyFrontend(final String message) {
 		ResponseMessage response = new ResponseMessage(message);
-		
+		notificationService.sendGlobalNotification();
 		messagingTemplate.convertAndSend("/topic/messages",response);
 		
 	}
 	
 	public void notifyUser(final String id, final String message) {
 		ResponseMessage response = new ResponseMessage(message);
-		
+		notificationService.sendPrivateNotification(id);
 		messagingTemplate.convertAndSendToUser(id, "/topic/private-messages", response);
 		
 	}
