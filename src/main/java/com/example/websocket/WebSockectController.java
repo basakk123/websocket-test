c8c3a504e8fc8ec248746aa299b41afd85a2a54d
dding ha <basakk@naver.com>
2022-10-03 16:21:00 +0900

    웹소켓 초기 세팅 완료

---
+++ src/main/java/com/example/websocket/WebSockectController.java
@@ -0,0 +1,29 @@
+package com.example.websocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import dto.Message;
+import dto.ResponseMessage;
+
+@Controller
+public class WebSockectController {
+	
+	@MessageMapping("/message")
+	@SendTo("/topic/messages")
+	public ResponseMessage getMessage(final Message message) {
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return new ResponseMessage(HtmlUtils.htmlEscape(message.getMessageContent()));
+		
+	}
+
+}
---
+++ src/main/java/dto/Message.java
@@ -0,0 +1,11 @@
+package dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Message {
+	private String messageContent;
+	
+}
